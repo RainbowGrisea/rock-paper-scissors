4e09bfe602e5813a68f02e3913871bb4223cfee7
Mark Kovacs <kovmark95@gmail.com>
2022-08-04 13:42:29 +0200

    Rework the game to make it playable with buttons

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 // the two choices are compared
 // rock beats scissors, scissors beats paper, paper beats rock
 
+
+const playersScoreText = document.querySelector('.playersScore');
+const computersScoreText = document.querySelector('.computersScore');
+const result = document.querySelector('.result');
+let playersScore = 0;
+let computersScore = 0;
+
 // Return the computers choice
 function getComputerChoice() {
     Math.floor(Math.random() * 3 + 1);
@@ -19,73 +26,76 @@ function getComputerChoice() {
     }
 }
 
-// Play a round of rock-paper-scissors
-function playARound() {
-    compChoice = getComputerChoice();
-    let usersChoice;
-    let incorrectInput = true;
-    // Exclude incorrect inputs
-    while (incorrectInput === true) {
-        usersChoice = prompt('Rock, paper or scissors?');
-        // Make the user's choice case insensitive
-        let firstLetter = usersChoice.slice(0, 1).toUpperCase();
-        let restLetters = usersChoice.slice(1).toLowerCase();
-        usersChoice = firstLetter + restLetters;
-        if (usersChoice === 'Rock' || usersChoice === 'Paper' || usersChoice === 'Scissors') {
-            incorrectInput = false;
-        } else {
-            console.log('Incorrect input! \nPlease make a proper choice!');
-        };
-    };
+// Make a choice with buttons
+const play = document.querySelectorAll('.playBtn');
+play.forEach(btns => {
+    btns.addEventListener('click', () => playARound(btns.textContent))
+});
 
+// Play a round of rock-paper-scissors
+function playARound(usersChoice) {
+    compChoice = getComputerChoice();
     switch (true) {
         case usersChoice === compChoice:
-            return ('It\'s a tie!');
+            result.textContent = 'It\'s a tie!';
             break;
         case usersChoice === 'Rock' && compChoice === 'Scissors':
-            return ('You win! Rock beats Scissors!');
+            result.textContent = 'You win! Rock beats Scissors!';
+            playersScore++;
             break;
         case usersChoice === 'Rock' && compChoice === 'Paper':
-            return ('You lose! Paper beats Rock!');
+            result.textContent = 'You lose! Paper beats Rock!';
+            computersScore++;
             break;
         case usersChoice === 'Paper' && compChoice === 'Rock':
-            return ('You win! Paper beats Rock!');
+            result.textContent = 'You win! Paper beats Rock!';
+            playersScore++;
             break;
         case usersChoice === 'Paper' && compChoice === 'Scissors':
-            return ('You lose! Scissors beats Paper');
+            result.textContent = 'You lose! Scissors beats Paper';
+            computersScore++;
             break;
         case usersChoice === 'Scissors' && compChoice === 'Rock':
-            return ('You lose! Rock beats Scissors!');
+            result.textContent = 'You lose! Rock beats Scissors!';
+            computersScore++;
             break;
         case usersChoice === 'Scissors' && compChoice === 'Paper':
-            return ('You win! Scissors beats Paper');
+            result.textContent = 'You win! Scissors beats Paper';
+            playersScore++;
             break;
-    }
-}
+    };
+    playersScoreText.textContent = ('You: ' + playersScore)
+    computersScoreText.textContent = ('Computer: ' + computersScore)
 
-// Play a game of 5 rock-paper-scissors
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    const regex = /You win!/i;
 
-    // Run until 5 games are won (ties don't count) 
-    for (let i = 0; i < 5; i++) {
-        let result = playARound();
-        console.log(result);
-        switch (true) {
-            case result === 'It\'s a tie!':
-                i--;
-                break;
-            case regex.test(result):
-                playerPoints++;
-                break;
-            default:
-                computerPoints++;
-        }
+    // Stop the game when a player reaches 5 points
+    switch (true) {
+        case playersScore === 5:
+            result.textContent = 'You WIN the game!!!';
+            break;
+        case computersScore === 5:
+            result.textContent = 'Computer WINS the game!!!';
+            break;
+    };
+
+    if (playersScore === 5 || computersScore === 5) {
+        play.forEach(btn => {
+            btn.disabled = true;
+        });
     }
-    // Who wins?
-    playerPoints > computerPoints ?
-        console.log('You WIN! \nYour points: ' + playerPoints + '\nComputer points: ' + computerPoints) :
-        // console.log('You LOSE! \nYour points: ' + playerPoints + '\nComputer points: ' + computerPoints);
-}
+};
+
+// Reset the game with reset button
+
+const resetBtn = document.querySelector('.resetBtn');
+resetBtn.addEventListener('click', () => {
+    playersScore = 0;
+    computersScore = 0;
+
+    play.forEach(btn => {
+        btn.disabled = false;
+    });
+    result.textContent = 'Let the game begin!';
+    playersScoreText.textContent = 'You:';
+    computersScoreText.textContent = 'Computer:';
+})
